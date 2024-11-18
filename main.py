07c90aee4686a3b785d0ab970f423664f1e4aff1
ricrudo <ricardood@gmail.com>
2024-11-18 06:54:18 -0500

    Pruebas basicas inicio de sesion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_from_directory
-from flask_jwt_extended import JWTManager, create_access_token,, create_refresh_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from pathlib import Path
 import json
 
